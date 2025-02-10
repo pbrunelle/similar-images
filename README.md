8ae7f7f27ce4065fec05f48e3ee0bd2a848d7177
Pierre Brunelle <pierrebrunelleai@gmail.com>
2025-02-10 01:12:43 +0000

    tmp path for 1 test

--- README.md
+++ README.md
@@ -6,8 +6,16 @@ Crawl for similar images
 EC2 Ubuntu:
 
 ```bash
+# Python
 sudo apt update
 sudo apt install python3.12-venv
+
+# AWS CLI
+sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+\rm -rf aws awscliv2.zip
 ```
 
 Then:
--- tests/test_db_client.py
+++ tests/test_db_client.py
@@ -6,18 +6,18 @@ from similar_images.db_client import DBClient
 def live_client():
     return DBClient()
 
-def test_store_image_data_happy_path(live_client):
+def test_store_image_data_happy_path(live_client, tmp_path):
     # GIVEN
     file_path="requirements.in"
     origin_url = "https://example.com/image.jpg"
-    tmppath = "testtmp"
+    out_path = tmp_path / "test_happy_path"
     # WHEN
     s3_url, image_id = live_client.upload(file_path=file_path, origin_url=origin_url)
-    item = live_client.download(file_path=tmppath, origin_url=origin_url)
+    item = live_client.download(file_path=out_path, origin_url=origin_url)
     # THEN
     print(item)
     expected_item = {'projectName': {'S': 'unit_tests'}, 'imageUrl': {'S': origin_url}, 's3Url': {'S': s3_url}, 's3Key': {'S': image_id}}
     assert item == expected_item
-    with open(tmppath, "r") as f1:
+    with open(out_path, "r") as f1:
         with open(file_path, "r") as f2:
             assert f1.read() == f2.read()
