fc99acdfd539bc55b96d798c592446c5be59d0f5
Pierre Brunelle <pierrebrunelleai@gmail.com>
2025-02-14 08:14:07 -0500

    gemini: dir

--- scripts/run_gemini.py
+++ scripts/run_gemini.py
@@ -2,12 +2,23 @@
 from similar_images.gemini import Gemini
 import httpx
 import fire
+import os
 
 async def run_gemini(query: str, image_paths: str):
     client = httpx.AsyncClient(follow_redirects=False, timeout=30)
     gemini = Gemini(httpx_client=client)
-    response = await gemini.chat(query, image_paths.split(","))
-    print(response)
+    decisions = []
+    if os.path.isdir(image_paths):
+        files = [os.path.join(image_paths, f) for f in os.listdir(image_paths) if os.path.isfile(os.path.join(image_paths, f))]
+    else:
+        files = image_paths.split(",")
+    print(files)
+    for image_path in files:
+        response = await gemini.chat(query, [image_path])
+        decision = response["candidates"][0]["content"]["parts"][0]["text"].strip().lower()
+        decisions.append((image_path, decision))
+    for image_path, decision in decisions:
+        print(f"{decision.ljust(4)} {image_path}")
 
 
 if __name__ == "__main__":
