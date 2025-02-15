a000cbb02fec4caf1ebbc43da38c61604af80057
Pierre Brunelle <pierrebrunelleai@gmail.com>
2025-02-15 10:02:08 -0500

    improve scrapper tests

--- tests/unit/test_scraper.py
+++ tests/unit/test_scraper.py
@@ -1,20 +1,26 @@
 from unittest.mock import AsyncMock, Mock
 
+import pytest
 from PIL import Image
 
 from similar_images.filters.image_filters import ImageFilter
 from similar_images.scraper import Scraper
 
 
-def test_apply_filters():
+@pytest.mark.parametrize(
+    "size,expected_keep,expected_code",
+    [
+        ((1000, 1000), True, None),
+        ((10, 10), False, "small"),
+    ],
+)
+def test_apply_filters(size, expected_keep, expected_code):
     # GIVEN
     scrapper = Scraper(browser=Mock(), client=AsyncMock())
-    image_filter = ImageFilter((100, 100), 50_000)
-    img = Image.new(mode="RGB", size=(1000, 1000))
+    filters = [ImageFilter((100, 100), 50_000)]
+    img = Image.new(mode="RGB", size=size)
     # WHEN
-    keep, explanation = scrapper.apply_filters(
-        url="http", img=img, filters=[image_filter]
-    )
+    keep, code = scrapper.apply_filters(url="http", img=img, filters=filters)
     # THEN
-    assert keep
-    assert not explanation
+    assert keep == expected_keep
+    assert code == expected_code
