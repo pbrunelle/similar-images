2c703550825d12140abb5b91f4b1898c4fc7ef7b
Pierre Brunelle <pierrebrunelleai@gmail.com>
2025-02-14 22:26:45 -0500

    Run gemini: write results to JSONL

--- scripts/run_gemini.py
+++ scripts/run_gemini.py
@@ -3,29 +3,29 @@ from similar_images.gemini import Gemini
 import httpx
 import fire
 import os
+import logging
 
-async def run_gemini(query: str, image_paths: str):
+logger = logging.getLogger(__name__)
+
+async def run_gemini(query: str, image_paths: str, outfile: str, model: str = "gemini-1.5-flash", max_output_tokens: int = 100):
+    logging.basicConfig(level=logging.DEBUG, force=True)
     client = httpx.AsyncClient(follow_redirects=False, timeout=30)
-    gemini = Gemini(httpx_client=client)
-    decisions = []
-    if os.path.isdir(image_paths):
-        files = [os.path.join(image_paths, f) for f in os.listdir(image_paths) if os.path.isfile(os.path.join(image_paths, f))]
-    else:
-        files = image_paths.split(",")
-    print(files)
-    for image_path in files:
-        print(image_path)
-        response = await gemini.chat(query, [image_path])
-        try:
-            decision = response["candidates"][0]["content"]["parts"][0]["text"].strip().lower()
-        except KeyError:
-            try:
-                decision = response["promptFeedback"]["blockReason"]
-            except KeyError:
-                decision = "<error>"
-        decisions.append((image_path, decision))
-    for image_path, decision in decisions:
-        print(f"{decision.ljust(20)} {image_path}")
+    gemini = Gemini(httpx_client=client, model=model, max_output_tokens=max_output_tokens)
+    files = []
+    for file in image_paths.split(","):
+        if os.path.isfile(file):
+            files.append(file)
+        elif os.path.isdir(file):
+            for subfile in os.listdir(file):
+                subpath = os.path.join(file, subfile)
+                if os.path.isfile(subpath):
+                    files.append(os.path.abspath(subpath))
+    logger.info(f"Processing: {len(files)}: {files}")
+    for file in files:
+        logger.debug(f"Processing: {file}")
+        decision = await gemini.chat(query, [file])
+        with open(outfile, "at") as f:
+            f.write(f"{decision.model_dump_json()}\n")
 
 
 if __name__ == "__main__":
--- similar_images/gemini.py
+++ similar_images/gemini.py
@@ -4,13 +4,34 @@ from typing import Any
 
 import httpx
 import asyncio
+from pydantic import BaseModel
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Decision(BaseModel):
+    image_path: str
+    content: dict[str, Any]
+    decision: str | None
+    status_code: int
 
 class Gemini:
-    def __init__(self, httpx_client: httpx.AsyncClient):
+    def __init__(self, httpx_client: httpx.AsyncClient, model: str, max_output_tokens: int):
         self._api_key = os.environ["GEMINI_API_KEY"]
         self._httpx_client = httpx_client
+        self._model = model
+        self.max_output_tokens = max_output_tokens
 
-    async def do_chat(self, query: str, image_paths: list[str]) -> dict[str, Any]:
+    async def chat(self, query: str, image_paths: list[str]) -> Decision:
+        for i in range(5):
+            try:
+                return await self.do_chat(query, image_paths)
+            except httpx.HTTPStatusError as ex:
+                logger.warning(f"Gemini failed {i=} on {image_paths=}: {type(ex)} {ex}")
+                await asyncio.sleep(10)
+        return Decision(image_path=image_paths[0], content={}, decision=None, status_code=400)
+    
+    async def do_chat(self, query: str, image_paths: list[str]) -> Decision:
         parts = [{"text": query}]
         for image_path in image_paths:
             with open(image_path, "rb") as f:
@@ -23,21 +44,23 @@ class Gemini:
                 }
                 parts.append(d)
         data = {
-            "generationConfig": {"max_output_tokens": 100},
+            "generationConfig": {"max_output_tokens": self.max_output_tokens},
             "contents": {"parts": parts},
         }
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={self._api_key}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/{self._model}:generateContent?key={self._api_key}"
         response = await self._httpx_client.post(url, json=data)
-        print(response)
-        print(response.content)
         response.raise_for_status()
-        return response.json()
+        return self.parse_response(image_path, response)
 
-    async def chat(self, query: str, image_paths: list[str]) -> dict[str, Any]:
-        for _ in range(5):
-            try:
-                return await self.do_chat(query, image_paths)
-            except httpx.HTTPStatusError as ex:
-                print(f"{type(ex)} {ex}")
-                asyncio.sleep(10)
-        return {}
+    def parse_response(self, image_path: str, response: httpx.Response) -> Decision:
+        content = response.json()
+        decision = None
+        try:
+            decision = content["promptFeedback"]["blockReason"]
+        except KeyError:
+            pass
+        try:
+            decision = content["candidates"][0]["content"]["parts"][0]["text"].strip().lower()
+        except (KeyError, IndexError):
+            pass
+        return Decision(image_path=image_path, content=content, decision=decision, status_code=response.status_code)
